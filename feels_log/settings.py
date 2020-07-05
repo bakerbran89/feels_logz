897664f176fb9b0671d75a68741e9e2d1f80ec92
bakerbran89 <bakerbran89@users.noreply.github.com>
2020-07-05 10:19:46 -0700

    fix

--- feels_log/settings.py
+++ feels_log/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8264@-icwh2*0_atrnyre_gd@n@+&r@75#gq_2u*&o%#^t#2wj'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
